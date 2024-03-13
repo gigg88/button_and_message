931a3595155ea180e0e69deb40b84bdd04fc60e8
Christian Popp <127033362+gigg88@users.noreply.github.com>
2024-03-13 09:53:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const text = "Hallo Dariia, bist du fleisig?";
 document.getElementById("clickButton").addEventListener("click", function() {
   document.getElementById("label").textContent = text;
 });
+
+const text1 = "Schick mir doch eine Nachricht zurück;)";
+
+document.getElementById("clickButton1").addEventListener("click", function() {
+  document.getElementById("label1").textContent = text1;
+});
